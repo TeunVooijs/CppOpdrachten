9c5455d937d68e789deacc290dc8251c9dea7c50
TeunVooijs <teunvooys@gmail.com>
2021-09-15 00:43:35 +0200

    Kan nu dagen van geboorte printen

--- opdracht1/Main.cpp
+++ opdracht1/Main.cpp
@@ -1,36 +1,212 @@
 #include "Headers/includes.h"
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
 
 int main(){
-    // int geboorte_jaar, geboorte_maand, geboorte_dag_getal, leeftijd_jaren, 
-    // leeftijd_maanden, leeftijd_dag;
-    // int dit_jaar=2020, deze_maand=9, deze_dag=9;
-    // cout << "Geef geboorte jaar: ";
-    // cin >> geboorte_jaar;
-    // cout << "Geef geboorte maand: ";
-    // cin >> geboorte_maand;
-    // cout << "Geef geboorte dag: ";
-    // cin >> geboorte_dag_getal;
+    int geb_jaar,geb_maand,geb_dag;
+    int jaar_nu=2021, maand_nu=9, dag_nu=13  ;
+    int leeftijd_jaar, leeftijd_maand, leeftijd_maanden;
+    int schrikkeljaren;
+    int dagen_geboorte_jaar, dagen_tussen_jaren, dagen_dit_jaar, dagen_oud;
+    int dag_geboren_pc_num;
+    char dag_geboren_pc;
 
-    // leeftijd_jaren=dit_jaar-geboorte_jaar;
-    // cout << leeftijd_jaren;
+    cout << "Geef alstublieft uw geboorte datum als volgt: jjjj mm dd\n";
+    cin >> geb_jaar >> geb_maand >> geb_dag;
 
+    cout << geb_jaar << " " << geb_maand << " " << geb_dag << "\n";
 
-    // if (geboorte_maand>deze_maand){
-    //     leeftijd_maanden = geboorte_maand-deze_maand;
-    //     leeftijd_jaren+=1;
-    // }
-    // else if(geboorte_maand==deze_maand){
-    //     if (deze_dag<geboorte_dag_getal){
-    //         leeftijd_jaren+=1
-    //     }
-    // }
-    // else if(geboorte_maand<deze_maand){
-    //     leeftijd_maanden = deze_maand-geboorte_maand;
-    // }
-     
-    // cout << leeftijd_jaren << geboorte_maand << geboorte_dag_getal;
+    if(geb_maand==(1,3,5,7,8,10,12) && geb_dag>31){
+        cout << "Dit is geen geldige geboorte datum\n";
+        return 1;
+    }
+    if(geb_maand==(4,6,9,11) && geb_dag>30){
+        cout << "Dit is geen geldige geboorte datum\n";
+        return 1;
+    }
+    if (geb_jaar % 4 ==0){
+        if(geb_maand==2 && geb_dag>29 ){
+            cout << "Dit is geen geldige geboorte datum\n";
+            return 1;
+        }
+    }
+    else if (geb_maand == 2 && geb_dag>28){
+        cout << "Dit is geen geldige geboorte datum\n";
+        return 1;
+    }
+
+    if (geb_jaar>2011 || geb_jaar<1921){
+        cout << "U bent te jong of te oud voor een universitaire studie\n";
+        return 1;
+    }
+    else{
+        cout << "Dit is een geldige leeftijd voor een universitaire studie\n";   
+    }
+
+    leeftijd_jaar= jaar_nu-geb_jaar;
+    leeftijd_jaar-=1;
+
+    if (geb_maand<maand_nu){
+        leeftijd_jaar+=1;
+    }
+    if (geb_maand==maand_nu){
+        if (geb_dag==dag_nu || geb_dag<dag_nu){
+            leeftijd_jaar+=1;
+        }
+        else{
+            leeftijd_jaar=leeftijd_jaar;
+        }
+    }
     
-    abc();
+    if(maand_nu>geb_maand){
+        leeftijd_maand=maand_nu-geb_maand;
+    }
+    else if (geb_maand>maand_nu){
+        leeftijd_maand=12-(geb_maand-maand_nu);
+    }
+    
+    leeftijd_maanden=(12*leeftijd_jaar)+leeftijd_maand;
+    
+    
+    cout << leeftijd_jaar << " Jaar en " << leeftijd_maand << " maanden oud\n" << "Ook wel: " << leeftijd_maanden << " maanden" << "\n";
+
+
+
+    //leeftijd in dagen voor dag uitrekenen
+
+    schrikkeljaren= (jaar_nu-geb_jaar)/4;
+    
+    
+    if(geb_jaar%4==2 || geb_jaar%4==3){
+        schrikkeljaren+=1;
+    }
+    cout << schrikkeljaren << "\n";
+    // telt alle dagen in je geboorte jaar
+
+    if (geb_jaar % 4 ==0 && geb_maand <=2){
+        if (geb_maand==1){
+            dagen_geboorte_jaar=366-geb_dag;
+        }
+        else if (geb_maand==2){
+            dagen_geboorte_jaar= 366-31-geb_dag;
+        }
+    }
+    if (geb_jaar % 4 != 0 || geb_maand >= 3){
+
+        if (geb_maand==1){
+            dagen_geboorte_jaar=365-geb_dag;
+        }
+        if (geb_maand==2){
+            dagen_geboorte_jaar= 365-31-geb_dag;
+        }
+        else if (geb_maand==3){
+            dagen_geboorte_jaar= 365-59-geb_dag;
+        }
+        else if (geb_maand==4){
+            dagen_geboorte_jaar= 365-90-geb_dag;
+        }
+        else if (geb_maand==5){
+            dagen_geboorte_jaar= 365-120-geb_dag;
+        }
+        else if (geb_maand==6){
+            dagen_geboorte_jaar= 365-151-geb_dag;
+        }
+        else if (geb_maand==7){
+            dagen_geboorte_jaar= 365-181-geb_dag;
+        }
+        else if (geb_maand==8){
+            dagen_geboorte_jaar= 365-212-geb_dag;
+        }
+        else if (geb_maand==9){
+            dagen_geboorte_jaar= 365-243-geb_dag;
+        }
+        else if (geb_maand==10){
+            dagen_geboorte_jaar= 365-273-geb_dag;
+        }
+        else if (geb_maand==11){
+            dagen_geboorte_jaar= 365-304-geb_dag;
+        }
+        else if (geb_maand==12){
+            dagen_geboorte_jaar= 365-334-geb_dag;
+        }
+    }      
+    
+    // Dit is voor de dagen in de jaren tussen dit jaar en het geboortejaar
+
+    
+    dagen_tussen_jaren=(schrikkeljaren*366)+((jaar_nu-geb_jaar-1-schrikkeljaren)*365);
+
+    // dagen voor dit jaar
+
+    if (maand_nu==1){
+        dagen_dit_jaar = dag_nu;
+    }
+    if (maand_nu==2){
+        dagen_dit_jaar=31+dag_nu;
+    }
+    if (maand_nu==3){
+        dagen_dit_jaar=59+dag_nu;
+    }
+    if (maand_nu==4){
+        dagen_dit_jaar=90+dag_nu;
+    }
+    if (maand_nu==5){
+        dagen_dit_jaar=120+dag_nu;
+    }
+    if (maand_nu==6){
+        dagen_dit_jaar=151+dag_nu;
+    }
+    if (maand_nu==7){
+        dagen_dit_jaar=181+dag_nu;
+    }
+    if (maand_nu==8){
+        dagen_dit_jaar=212+dag_nu;
+    }
+    if (maand_nu==9){
+        dagen_dit_jaar=243+dag_nu;
+    }
+    if (maand_nu==10){
+        dagen_dit_jaar=273+dag_nu;
+    }
+    if (maand_nu==11){
+        dagen_dit_jaar=304+dag_nu;
+    }
+    if (maand_nu==12){
+        dagen_dit_jaar=334+dag_nu;
+    }
+
+    if (jaar_nu%4==0 && maand_nu>2){
+        dagen_dit_jaar+=1;
+    }
+    
+    dagen_oud=dagen_geboorte_jaar + dagen_tussen_jaren +dagen_dit_jaar;
+    cout << dagen_oud << "\n";
+    dag_geboren_pc_num= (dagen_oud % 7);
+    
+    if (dag_geboren_pc_num == 0){
+        cout << "m";    // maandag geboren
+    }
+    else if (dag_geboren_pc_num == 6){
+        cout << "di";    // dinsdag geboren 
+    }
+    else if (dag_geboren_pc_num == 5){
+        cout << "wo";    // woensdag geboren
+    }
+    else if (dag_geboren_pc_num == 4){
+        cout << "do";   // donderdag geboren
+    }
+    else if (dag_geboren_pc_num == 3){
+        cout << "v";    // vrijdag geboren
+    }
+    else if (dag_geboren_pc_num == 2){
+        cout << "za";   // zaterdag geboren
+    }
+    else if (dag_geboren_pc_num == 1){
+        cout << "zo";   // zondag geboren
+    }
+
     
     return 0;
 }
